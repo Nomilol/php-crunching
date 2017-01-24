eff42204b9a6a9f13821b6e048b827588963e71c
Axel <lezinaxel@gmail.com>
2017-01-24 16:34:48 +0100

    Affichage du nombre de mots qui contiennent la lettre w

--- index.php
+++ index.php
@@ -4,12 +4,21 @@ $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 
 $dicoLentgh = sizeof($dico);
-//var_dump($dico);
-//echo $dicoLentgh;
-$array15 = [];
+echo $dicoLentgh;
+ $array15 = [];
+ foreach ($dico as $key => $value) {
+   if (strlen($value) === 15) {
+     array_push($array15,$value);
+   }
+ }
+ echo sizeof($array15);
+
+
+$arrayS = [];
+
 foreach ($dico as $key => $value) {
-  if (strlen($value) === 15) {
-    array_push($array15,$value);
+  if(strpos($value, "w")!=false){
+    array_push($arrayS, $value);
   }
 }
-echo sizeof($array15);
+echo sizeof($arrayS);
