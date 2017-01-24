4e00909d536e086b1ccb95272d11ea2a217e1116
Axel <lezinaxel@gmail.com>
2017-01-24 14:51:49 +0100

    Affichage du nombre de mots ayant 15 caractères

--- index.php
+++ index.php
@@ -4,4 +4,12 @@ $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 
 $dicoLentgh = sizeof($dico);
-echo $dicoLentgh;
+//var_dump($dico);
+//echo $dicoLentgh;
+$array15 = [];
+foreach ($dico as $key => $value) {
+  if (strlen($value) === 15) {
+    array_push($array15,$value);
+  }
+}
+echo sizeof($array15);
