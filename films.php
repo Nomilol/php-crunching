e7b329a0fcb0e21c4b3c10c24cbe2650edc65d06
Axel <lezinaxel@gmail.com>
2017-01-25 10:12:46 +0100

    Affichage de la position du film Gravity

--- films.php
+++ films.php
@@ -1,14 +1,26 @@
 <?php
 
-$string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
-$brut = json_decode($string, true);
-$top = $brut["feed"]["entry"];
+ $string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
+ $brut = json_decode($string, true);
+ $top = $brut["feed"]["entry"];
+
+ // var_dump($top);
+ //
+ // function ten($index){
+ //   for ($i=0; $i < 10 ; $i++) {
+ //     echo '<li>' . $index[$i]['im:name']['label'] . '</li>';
+ //   }
+ // }
+ // echo ten($top);
 
 
 
-function ten($index){
-  for ($i=0; $i < 10 ; $i++) {
-    echo '<li>' . $index[$i]['im:name']['label'] . '</li>';
+function getGravity($index, $searching){
+  for ($i= 0 ; $i < 100 ; $i++) {
+    if($index[$i]['im:name']['label'] === $searching){
+      return $i + 1;
+    }
   }
 }
-echo ten($top);
+
+echo getGravity($top,'Gravity');
